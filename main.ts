f200cc56be2b3ab87144f6af3198057a0069b6ce
louise780 <91077909+louise780@users.noreply.github.com>
2021-11-01 14:30:33 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let start = 0
+let elapsed = 0
+input.onButtonPressed(Button.A, function () {
+    start = input.runningTime()
+})
+input.onButtonPressed(Button.B, function () {
+    elapsed = input.runningTime() - start
+    basic.showNumber(Math.idiv(elapsed, 1000))
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
